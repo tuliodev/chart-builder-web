37303f8f1b973ade8d006ffde1430c07de8f86d9
tuliodev <resendetulio51@gmail.com>
2024-01-23 22:32:24 -0300

    feature:  make header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Image from "next/image";
 
 import chevronDownIcon from "@/assets/img/icons/chevron-down.svg";
@@ -7,13 +8,20 @@ import userIcon from "@/assets/img/icons/user-icon.svg";
 import logoImg from "@/assets/img/logo.svg";
 
 export default function Header() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="bg-primary-blue p-5 flex flex-row justify-between">
       <div className="flex flex-row gap-8 items-center">
         <Image priority src={logoImg} height={80} width={80} alt="Logo" />
-        <p className="text-white cursor-pointer hover:opacity-75">Boards</p>
-
-        <p className="text-white flex flex-row items-center gap-1  cursor-pointer hover:opacity-75">
+        <p className="hidden sm:flex text-white cursor-pointer hover:opacity-75">
+          Boards
+        </p>
+        <p className="text-white hidden sm:flex flex-row items-center gap-1 cursor-pointer hover:opacity-75">
           Reports{" "}
           <Image
             priority
@@ -25,7 +33,7 @@ export default function Header() {
         </p>
       </div>
 
-      <div className="flex flex-row gap-6 items-center ">
+      <div className="hidden sm:flex flex-row gap-6 items-center">
         <Image
           priority
           src={helpCircleIcon}
@@ -34,7 +42,6 @@ export default function Header() {
           alt="Help Circle"
           className="cursor-pointer hover:opacity-75"
         />
-
         <Image
           priority
           src={settingsIcon}
@@ -43,7 +50,6 @@ export default function Header() {
           alt="Settings"
           className="cursor-pointer hover:opacity-75"
         />
-
         <p className="text-white flex flex-row items-center gap-2">
           <Image
             priority
@@ -64,6 +70,70 @@ export default function Header() {
           </span>
         </p>
       </div>
+
+      <div className="sm:hidden flex items-center">
+        <button onClick={toggleMenu} className="text-white"></button>
+        <button
+          type="button"
+          className="inline-flex items-center md:hidden"
+          onClick={toggleMenu}
+        >
+          {isMenuOpen ? (
+            ""
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="40"
+              height="40"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="#fff"
+                d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
+
+      {isMenuOpen && (
+        <div className="sm:hidden flex flex-col gap-3 items-end ">
+          <button onClick={toggleMenu} className="text-white mb-2 text-2xl">
+            X
+          </button>
+          <p className="text-white cursor-pointer hover:opacity-75">Boards</p>
+          <p className="text-white cursor-pointer hover:opacity-75 flex items-center gap-1">
+            Reports{" "}
+          </p>
+          <div className="flex flex-row gap-1">
+            <Image
+              priority
+              src={helpCircleIcon}
+              height={26}
+              width={26}
+              alt="Help Circle"
+              className="cursor-pointer hover:opacity-75"
+            />
+            <Image
+              priority
+              src={settingsIcon}
+              height={26}
+              width={26}
+              alt="Settings"
+              className="cursor-pointer hover:opacity-75"
+            />
+          </div>
+          <p className="text-white flex flex-row items-center gap-2">
+            <Image
+              priority
+              src={userIcon}
+              height={40}
+              width={40}
+              alt="User Icon"
+            />
+          </p>
+        </div>
+      )}
     </header>
   );
 }
